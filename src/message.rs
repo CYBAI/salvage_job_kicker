d05e4064ab38b436c18dcd2a7a5239f6795b7c73
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-09-10 22:18:21 +0900

    Correct field name

--- src/message.rs
+++ src/message.rs
@@ -3,7 +3,7 @@ use super::media_linkage_credential::MediaLinkageCredential;
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Message<'a> {
-    pub id: String,
+    pub media_linkage_id: String,
     pub vault_path: String,
     pub company_id: String,
     pub media: String,
@@ -13,7 +13,7 @@ pub struct Message<'a> {
 impl<'a> Message<'a> {
     pub fn from_media_linkage_credential(cred: MediaLinkageCredential, action: &'a str) -> Self {
         return Message {
-            id: cred.id,
+            media_linkage_id: cred.id,
             vault_path: cred.vault_path,
             company_id: cred.company_id,
             media: cred.media,
